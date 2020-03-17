e7a1eb60b11e22e0e6d49ddb0cdf2a0329963687
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-03-16 23:38:12 -0300

    removing pucharse quantity

--- app/models/pucharse.rb
+++ app/models/pucharse.rb
@@ -1,6 +1,4 @@
 class Pucharse < ApplicationRecord
-  belongs_to :category		
-
   validates :name, uniqueness: true, presence: true
   validates_presence_of :price
 end
--- db/migrate/20200316193414_create_pucharses.rb
+++ db/migrate/20200316193414_create_pucharses.rb
@@ -1,7 +1,6 @@
 class CreatePucharses < ActiveRecord::Migration[5.0]
   def change
     create_table :pucharses do |t|
-      t.references :category, foreign_key: true
       t.string :name
       t.float :price
 
