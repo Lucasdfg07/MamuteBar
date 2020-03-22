a379bf0d050b6d14b579de39bf50bd594b06624e
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-03-22 12:14:32 -0300

    bar example chart

--- app/assets/javascripts/sales.js
+++ app/assets/javascripts/sales.js
@@ -1,20 +1,26 @@
 $( document ).on('turbolinks:load', function() {
-  new Morris.Line({
-    element: 'requests_chart',
-    data: $('#requests_chart').data('requests'),
-    xkey: 'created_at',
-    ykeys: ['received', 'spent'],
-    labels: ['Faturado (R$)','Quantia Gasta (R$)'],
-    lineColors: ['#007bff', '#DC143C']
-  });
+  // new Morris.Line({
+  //   element: 'requests_chart',
+  //   data: $('#requests_chart').data('requests'),
+  //   xkey: 'created_at',
+  //   ykeys: ['received', 'spent'],
+  //   labels: ['Faturado (R$)','Quantia Gasta (R$)'],
+  //   lineColors: ['#007bff', '#DC143C']
+  // });
 
   Morris.Bar({
-    element: 'payment_chart',
-    data: $('#payment_chart').data('requests'),
-    xkey: 'created_at',
-    ykeys: ['cash', 'card'],
-    labels: ['Dinheiro', 'Cartão']
-  }).on('click', function(i, row){
-    console.log(i, row);
-  });
+        element: 'bar-example',
+        data: [
+            { y: '2006', a: 100, b: 90 },
+            { y: '2007', a: 75, b: 65 },
+            { y: '2008', a: 50, b: 40 },
+            { y: '2009', a: 75, b: 65 },
+            { y: '2010', a: 50, b: 40 },
+            { y: '2011', a: 75, b: 65 },
+            { y: '2012', a: 100, b: 90 }
+        ],
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        labels: ['Series A', 'Series B']
+    });
 });
