fef0e52002bb142014a4c01c4565158bc1e9ad8a
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-03-22 12:09:05 -0300

    alter graph

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,6 +1,21 @@
 module SalesHelper
 	def requests_chart_data
-		
+		(3.month.ago.to_date..Date.today).map do |date| 
+			total = 0
+			Request.where("date(created_at) = ? AND status = 'closed'", date).each{ |r| 
+				count = 0
+				r.products.each{|product|
+					product_price = Product.find(product.to_i).price
+					total += product_price * r.quantity[count]
+					count += 1
+				}
+			}
+			{
+				created_at: date,
+				received: total,
+				spent: Pucharse.where("date(created_at) = ?", date).sum("price")
+			}
+		end
 	end
 
 	def payment_data
